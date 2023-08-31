ff71cd2d56325cb6c329328ed4667a3192d6f487
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-08-31 15:18:52 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,74 +1,102 @@
-import http, { request } from "node:http";
-import fs from "fs/promises"
-import { stringify } from "node:querystring";
-import { writeFile } from "node:fs";
+    import http, { request } from "node:http";
+    import fs from "fs/promises"
+    import { stringify } from "node:querystring";
+    import { writeFile } from "node:fs";
 
+    import express from "express";
+    import cors from "cors"
 
-    // er det ikke bedre at lave den asyncron? videoen sagde man kunne ende i callback hell
+        // er det ikke bedre at lave den asyncron? videoen sagde man kunne ende i callback hell
 
-const app = http.createServer(async (request, response) => {
-    if (request.url === "/" && request.method==="GET") {
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
 
-response.statusCode = 200; //bestemme om HTTP request er fuldført
-response.setHeader("Content-Type", "text/plain"); //fortæller server om datatype den ønsker som response
- response.end("Working with HTTP");
-}
-else if(request.url==="/users" && request.method==="GET"){
-      response.statusCode = 200; //bestemme om HTTP request er fuldført
-      response.setHeader("Content-Type", "application/json"); //fortæller server om datatype den ønsker som response
-     
-      const json= await fs.readFile("data/users.json")
-
-     response.end(json);
-    } 
+    app.get("/users", async (req,res) => {
     
-    else if(request.url==="/posts" && request.method==="GET"){
-        response.statusCode = 200; //bestemme om HTTP request er fuldført
-        response.setHeader("Content-Type", "application/json"); //fortæller server om datatype den ønsker som response
-       
+        const json= await fs.readFile("data/users.json")
 
-       const json= await fs.readFile("data/posts.json")
-
-        response.end(json);
-    }
-    else if (request.url==="/users" && request.method==="POST"){
+        res.send(json);
+    })  
         
+    app.get("/posts", async (req,res) => {
+    
+        const json= await fs.readFile("data/posts.json")
 
-       const user = {
-        id: new Date().getTime(),
-        image: "url",
-        mail: "tester@test.dk",
-        name: "test user",
-        title: "Tester"
-       };
+        res.send(json);
+    }) 
+        
+        
+                
+            app.post("/users", async (req,res)=>{
 
-       // læser fra JSON
-       const json = await fs.readFile("data/users.json");
+            const user = {
+              id: new Date().getTime(),
+              image: req.body.image,
+              mail: req.body.mail,
+              name: req.body.name,
+              title: req.body.title,
+            };
 
-       //Parse til javascript
+            
+ 
+
+        // læser fra JSON
+        const json = await fs.readFile("data/users.json");
+
+        //Parse til javascript
+        
+            const users = JSON.parse(json);
        
-        const users = JSON.parse(json);
-       console.log(users)
 
-       users.push(user);
+        users.push(user);
 
-       //konverterer users til JSON format
-       const usersJson = JSON.stringify(users);
+        //konverterer users til JSON format
+        const usersJson = JSON.stringify(users);
 
-       //skriver til JSON fil - dvs tilføjer vores nye object
-       await fs.writeFile("data/users.json", usersJson);
+        //skriver til JSON fil - dvs tilføjer vores nye object
+        await fs.writeFile("data/users.json", usersJson);
 
-       response.statusCode = 200;
-       response.setHeader("Content-Type", "application/json");
-
-       response.end(usersJson);
-    };
-
+       
+        //vise restultatet dvs hele listen
+        res.send(usersJson);
+            })
     
-    
-});
+            
 
-const port = 3000;
-app.listen(port, ()=> {
-    console.log(`Serveren kører på http://localhost:${port}`);
-});
+              
+            app.post("/posts", async (req, res) => {
+
+
+              const post = {
+                
+              id: new Date().getTime(),
+              caption: req.body.caption,
+              createdAt: req.body.createdAt,
+              image: req.body.image,
+              uid: req.body.uid,
+            };
+   
+              // læser fra JSON
+              const json = await fs.readFile("data/posts.json");
+
+              //Parse til javascript
+
+              const posts = JSON.parse(json);
+
+              posts.push(post);
+
+              //konverterer users til JSON format
+              const postsJson = JSON.stringify(posts);
+
+              //skriver til JSON fil - dvs tilføjer vores nye object
+              await fs.writeFile("data/posts.json", postsJson);
+
+              //vise restultatet dvs hele listen
+              res.send(postsJson);
+            });
+
+    const port = 3000;
+    app.listen(port, ()=> {
+        console.log(`Serveren kører på http://localhost:${port}`);
+    });
